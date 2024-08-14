d589f9f1211622b34dd8b60768fb038fae99cb5d
Kyle Mardell <kyleslufc@gmail.com>
2024-08-14 21:06:31 +0000

    Set the SECRET_KEY in the os environment for security and Heroku deployment.

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p+%n@u3tf^7n1ni0dk!u7r8&c+3+5q%2*+y*5y5_1$=!f-ypij'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
