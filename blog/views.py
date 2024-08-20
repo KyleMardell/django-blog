70d9da32061b605a210449cc634cef2ef2fffb89
Kyle Mardell <kyleslufc@gmail.com>
2024-08-20 21:56:12 +0000

    Add code to edit a comment as a user.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
 
-# Create your views here.
 
 class PostList(generic.ListView):
     queryset = Post.objects.all()
@@ -16,12 +16,10 @@ def post_detail(request, slug):
     Display an individual :model:`blog.Post`.
 
     **Context**
-
     ``post``
         An instance of :model:`blog.Post`.
 
     **Template:**
-
     :template:`blog/post_detail.html`
     """
 
@@ -42,7 +40,9 @@ def post_detail(request, slug):
                 'Comment submitted and awaiting approval'
             )
 
+
     comment_form = CommentForm()
+
 
     return render(
         request,
@@ -55,3 +55,26 @@ def post_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
